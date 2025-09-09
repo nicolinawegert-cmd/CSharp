4d5e5a254a4fc3a9a439ad31aa91c962f02ee539
Nicolina Wegert <nicolinawegert@MacBook-Air-som-tillhor-Nicolina.local>
2025-09-09 21:38:58 +0200

    Added my projects

---
+++ Project2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hej världen!");
+            Console.WriteLine("Mitt namn är Nicolina");
+            Console.WriteLine("Idag är det " + DateTime.Now.ToShortDateString());
+
+            string favoritFärg = "Blå";
+            Console.WriteLine("Min favoritfärg är " + favoritFärg);
+
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine();
+            Console.WriteLine("Hej, " + namn + "!");
+        }
+    }
+}
