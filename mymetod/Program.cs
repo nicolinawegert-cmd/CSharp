c967e684fff6fcccaac42a7ec5e3c03d543225e3
Nicolina Wegert <nicolinawegert@MacBook-Air-som-tillhor-Nicolina.local>
2025-09-16 13:19:30 +0200

    Lagt till SayHello-metod

---
+++ mymetod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program  
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ditt namn: ");
+        string name = Console.ReadLine();
+        SayHello(name);
+        SayGoodbye(name);
+    
+    }
+    static void SayHello(string name)
+        {
+            Console.WriteLine($"Hej {name}, välkommen till programmet!");
+        }
+    static void SayGoodbye(string name)
+        {
+            Console.WriteLine($"Hejdå {name}, hoppas vi ses igen!");
+        }
+}
